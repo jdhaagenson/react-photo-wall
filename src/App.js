f964bcb31590e6e622b20e895029b041c39c69a6
Jordan Haagenson <jdhaagenson@gmail.com>
2020-02-04 11:29:45 -0800

    get photos to display

--- src/App.js
+++ src/App.js
@@ -9,13 +9,23 @@ const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {};
+  state = {
+    photos: []
+  };
 
   // 2. Declare a life cycle method
   // This life cycle method should:
   //  - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
+  componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then(response => response.json()
+      .then(responseBody => {
+        console.log(responseBody)
+        this.setState({photos: responseBody});
+      }));
+  }
   render() {
     const { photos = [] } = this.state;
     return (
@@ -32,11 +42,11 @@ class App extends Component {
           {/* We use map here because Array.prototype.map is an expression,
            * and for loops are not. You'll learn more about this soon!
            */}
-          {photos.map(photo => (
+          {this.state.photos.map(photo => (
             <img
-              alt={/* 3. Fill me in with the photo's filename */ ""}
-              key={/* 4. Fill me in with the photo's id */ ""}
-              src={/* 5. Fill me in with the photo's URL */ ""}
+              alt={/* 3. Fill me in with the photo's filename */ photo.filename}
+              key={/* 4. Fill me in with the photo's id */ photo.id}
+              src={/* 5. Fill me in with the photo's URL */ PHOTO_URL(photo.id)}
             />
           ))}
         </div>
